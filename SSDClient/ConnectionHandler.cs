bef066b86896256d7d029cdf75d039f1fc566070
Fabidini <Fabi_TTT@outlook.de>
2025-02-04 09:41:52 +0100

    Moved Models from Client to API library

--- SSDClient/ConnectionHandler.cs
+++ SSDClient/ConnectionHandler.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using SSDClient.Models;
 using SSDServer;
 
 namespace SSDClient
@@ -18,13 +19,20 @@ namespace SSDClient
             _address = address;
             _client = new Client();
             _client.connect(address);
-
+           
         }
 
         public void login(string username, string password)
         {
             SHA256 sha = SHA256.Create();
             byte[] hash = sha.ComputeHash(BitConverter.GetBytes(password.GetHashCode()));
+            ClientPackage clientPackage = new ClientPackage(ClientPackage.clientPackageID.Login, new AccountInfo(0, username, 0, hash), new RequestInfo(Guid.Empty, "", "", ""));
+            sendToServer(clientPackage.toByteArray());
+        }
+
+        public void sendToServer(byte[] payload) 
+        { 
+            _client.stream.Write(payload, 0, payload.Length);
         }
     }
 }
